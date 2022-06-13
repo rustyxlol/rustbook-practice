4f8130501d409adaf804df9496d2c2a061bdcb1a
Rusty <imkhazix@gmail.com>
2022-06-13 22:08:49 +0530

    restaurant module

---
+++ modules/restaurant/src/lib.rs
@@ -0,0 +1,22 @@
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+
+        pub fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+
+        fn serve_order() {}
+
+        fn take_payment() {}
+    }
+}
+
+
+pub fn eat_at_restaurant() {
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    front_of_house::hosting::seat_at_table();
+}
