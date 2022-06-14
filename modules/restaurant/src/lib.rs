4d4a939e6d8028cdfbd1dd2e54b52fb8dffeffaf
Rusty <imkhazix@gmail.com>
2022-06-14 14:14:06 +0530

    restaurant with both front and back in one module

--- modules/restaurant/src/lib.rs
+++ modules/restaurant/src/lib.rs
@@ -14,9 +14,29 @@ mod front_of_house {
     }
 }
 
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast { 
+                toast: String::from(toast), 
+                seasonal_fruit: String::from("Peaches"),
+            }
+        }
+    }
+}
 
 pub fn eat_at_restaurant() {
-    crate::front_of_house::hosting::add_to_waitlist();
+    let mut meal = back_of_house::Breakfast::summer("Wheat");
+    meal.toast = String::from("Rye");
 
-    front_of_house::hosting::seat_at_table();
+    println!("I'd like {} toast please", meal.toast);
+
+    // meal.seasonal_fruit = String::from("blueberries");
+    // Wont work because it isn't public
+
 }
