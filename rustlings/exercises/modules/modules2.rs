9603032e6887beb87452451707f6fe288477fcd6
Rusty <imkhazix@gmail.com>
2022-06-14 14:31:50 +0530

    completed modules

--- rustlings/exercises/modules/modules2.rs
+++ rustlings/exercises/modules/modules2.rs
@@ -3,15 +3,16 @@
 // 'use' and 'as' keywords. Fix these 'use' statements to make the code compile.
 // Make me compile! Execute `rustlings hint modules2` for hints :)
 
-// I AM NOT DONE
 
 mod delicious_snacks {
 
-    // TODO: Fix these use statements
-    use self::fruits::PEAR as ???
-    use self::veggies::CUCUMBER as ???
+    // pub use self::fruits::PEAR as fruit;
+    // Removed above statement to see the difference a pub use can make
+    // if you have pub use, you don't need a module to be public
+    
+    pub use self::veggies::CUCUMBER as veggie;
 
-    mod fruits {
+    pub mod fruits {
         pub const PEAR: &'static str = "Pear";
         pub const APPLE: &'static str = "Apple";
     }
@@ -25,7 +26,7 @@ mod delicious_snacks {
 fn main() {
     println!(
         "favorite snacks: {} and {}",
-        delicious_snacks::fruit,
+        delicious_snacks::fruits::PEAR,
         delicious_snacks::veggie
     );
 }
