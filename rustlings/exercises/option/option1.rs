94523ae56a6d912442ee2ecfdb4f7e49bfa5c4bb
Rusty <imkhazix@gmail.com>
2022-06-23 19:27:34 +0530

    options

--- rustlings/exercises/option/option1.rs
+++ rustlings/exercises/option/option1.rs
@@ -1,23 +1,22 @@
 // option1.rs
 // Make me compile! Execute `rustlings hint option1` for hints
 
-// I AM NOT DONE
 
-// you can modify anything EXCEPT for this function's signature
-fn print_number(maybe_number: Option<u16>) {
+// you can modify anything except for this function's signature
+fn print_number(maybe_number: option<u16>) {
     println!("printing: {}", maybe_number.unwrap());
 }
 
 fn main() {
-    print_number(13);
-    print_number(99);
+    print_number(some(13));
+    print_number(some(99));
 
-    let mut numbers: [Option<u16>; 5];
+    let mut numbers: [option<u16>; 5] = [some(0); 5];
     for iter in 0..5 {
         let number_to_add: u16 = {
             ((iter * 1235) + 2) / (4 * 16)
         };
 
-        numbers[iter as usize] = number_to_add;
+        numbers[iter as usize] = Some(number_to_add);
     }
 }
