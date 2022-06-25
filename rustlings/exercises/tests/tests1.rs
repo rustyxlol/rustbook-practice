e1b2da96d2aaefa0df81b202da372eae8f67a923
Rusty <imkhazix@gmail.com>
2022-06-26 01:19:48 +0530

    Finished tests

--- rustlings/exercises/tests/tests1.rs
+++ rustlings/exercises/tests/tests1.rs
@@ -6,12 +6,15 @@
 // This test has a problem with it -- make the test compile! Make the test
 // pass! Make the test fail! Execute `rustlings hint tests1` for hints :)
 
-// I AM NOT DONE
+fn adder_two(num: i32) -> i32 {
+    num + 2
+}
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
     fn you_can_assert() {
-        assert!();
+        assert!(4 == adder_two(2));
     }
 }
